806bff362a00ce276e320b31b531089c12289071
Eshita Bhattacharjee <bhattacharyaeshita@gmail.com>
2023-01-07 22:25:56 +0530

    D7

--- Python/Arrays/MajorityElement.py
+++ Python/Arrays/MajorityElement.py
@@ -3,14 +3,14 @@ class Solution:
         n=len(nums)
         cache={} #key=num : value= count
         
-        for i in nums:
+        for i in nums: #keeping count of occurence of every element
             if i in cache.keys():
                 cache[i]+=1
             else:
                 cache[i]=1
         
-        a=list(cache.keys())
+        a=list(cache.keys()) #making a list of all distinct elements
         for j in a:
-            if cache[j]>int(n/2):
+            if cache[j]>int(n/2): #we return the element whose count is greater than n/2
                 return j
         return -1
